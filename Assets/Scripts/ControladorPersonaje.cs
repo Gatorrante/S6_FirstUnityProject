5f28cabbce650e528630bffe83a9daf5702b1ba5
Diegokjasd <68989455+Gatorrante@users.noreply.github.com>
2024-08-25 14:10:57 -0500

    Added the logic and programming
    Before of that there was just the build for web

---
+++ Assets/Scripts/ControladorPersonaje.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorPersonaje : MonoBehaviour
+{
+    public float velocidadMovimiento;
+
+    public Transform detectorSuelo;
+    bool tocandoSuelo;
+    public float fuerzaSalto;
+    public LayerMask suelo;
+    public float rangoSuelo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float controlHorizontal=Input.GetAxis("Horizontal");
+        GetComponent<Rigidbody2D>().velocity=new Vector3(
+            controlHorizontal*velocidadMovimiento,GetComponent<Rigidbody2D>().velocity.y);
+
+
+        tocandoSuelo=Physics2D.OverlapCircle(detectorSuelo.position, rangoSuelo, suelo);
+        if(tocandoSuelo) {
+            GetComponent<Rigidbody2D>().velocity=new Vector2(0,0);
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, fuerzaSalto));
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/Limites.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Limites : MonoBehaviour
+{
+    public GameObject Borde1;
+    public GameObject Borde2;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.x>Borde1.transform.position.x) { //Si la posición en X de mi personaje es mayor que el Borde1
+
+        this.transform.position = new Vector3(Borde2.transform.position.x,
+        this.transform.position.y, this.transform.position.z);
+//Mueva a la posicion del Borde2
+    }
+        if(this.transform.position.x<Borde2.transform.position.x)//Si la posiciónen X de mi personaje es menor que el Borde2
+{
+            this.transform.position = new Vector3(Borde1.transform.position.x,
+            this.transform.position.y, this.transform.position.z);
+}
+ 
+    }
+}
